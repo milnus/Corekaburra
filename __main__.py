6bd46fa93f62b1565893713d238bd16f6b2fc600
Magnus Jespersen <magnus.ganer.j@gmail.com>
2020-10-26 12:27:24 +1100

    small changes made to gff_parser and functions added to main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 from .parse_gene_presence_absence import read_gene_presence_absence
 
-
-def get_arguments():
+#def get_arguments():
     # TODO - Set up arguments parser
     # ARGUMENTS:
     #
@@ -9,13 +8,15 @@ def get_arguments():
 def main():
     # TODO - Get arguments
 
-    # TODO - Open gene_presence_absence file and return dict with a key for each core gene cluster and all locus_tags as the value for each key.
-    core_dict, low_freq_dict = read_gene_presence_absence(#TODO - Give arguments)
-
     # TODO Check if all gff files are present in input folder
 
-    # TODO for-loop over each gff
-    # TODO Parse gff
+    # TODO - Open gene_presence_absence file and return dict with a key for each core gene cluster and all locus_tags as the value for each key.
+    core_dict, low_freq_dict = read_gene_presence_absence(# TODO - Give arguments)
+
+    # TODO for-loop over each gff - Try to multiprocess
+    # TODO Parse gff and extract core and low frequency genes from gffs
+
+    # TODO Get all results and mash together into one.
 
     ### FUNCTION ###
     # TODO Get the synteny of genes if genome is complete with score 1-n_core_genes
