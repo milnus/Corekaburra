95efe3a90fe72c3c8196b38d283c851e7f62d46e
Magnus Jespersen <magnus.ganer.j@gmail.com>
2021-01-30 13:41:17 +1100

    Fix the logic in determining the source program of the pan genome

--- check_inputs.py
+++ check_inputs.py
@@ -9,22 +9,30 @@ def check_gff_files(file_list):
 
 
 def define_input_source(folder):
+    """ Function to examine if input pan genome folder stems from Roary or Panaroo.
+    Returns the program that is the source and the path to the right gene presence/absence file """
     try:
-        with open(os.path.join(folder, 'gene_presence_absence.csv'), 'r') as gene_pres_abs:
-            if '"' in gene_pres_abs.readline():
-                gene_pres_abs_file_path = os.path.join(folder, 'gene_presence_absence.csv')
-                return "Roary", gene_pres_abs_file_path
-            else:
-                gene_pres_abs.close()
-                gene_pres_abs_file_path = os.path.join(folder, 'gene_presence_absence_roary.csv')
-                if os.path.isfile(gene_pres_abs_file_path):
-                    return "Panaroo", gene_pres_abs_file_path
+        if os.path.isfile(os.path.join(folder, 'gene_presence_absence.csv')):
+            # See if input is from Roary
+            with open(os.path.join(folder, 'gene_presence_absence.csv'), 'r') as gene_pres_abs:
+                if '"' in gene_pres_abs.readline():
+                    gene_pres_abs_file_path = os.path.join(folder, 'gene_presence_absence.csv')
+                    return "Roary", gene_pres_abs_file_path
+
+            gene_pres_abs.close()
+
+        # See if input is from Panaroo
+        print('PAN')
+        gene_pres_abs_file_path = os.path.join(folder, 'gene_presence_absence_roary.csv')
+        if os.path.isfile(gene_pres_abs_file_path):
+            return "Panaroo", gene_pres_abs_file_path
 
     except FileNotFoundError:
         raise FileNotFoundError('No gene presence absence file was found in given pan genome folder')
 
 
 def check_gene_data(folder):
+    """ Check if the gene_data.csv file is pressent in the folder from a Panaroo pan genome run. """
     if os.path.isfile(os.path.join(folder, 'gene_data.csv')):
         return True
     else:
