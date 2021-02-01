1e199fcfc5ca912d52e070717c32dade67aa6a24
Magnus Jespersen <magnus.ganer.j@gmail.com>
2021-02-02 06:32:01 +1100

    Fully working implementation of adding in refound genes into gff files for Panaroo

--- correct_gffs.py
+++ correct_gffs.py
@@ -1,9 +1,11 @@
 from Bio import Seq
 from Bio.SeqRecord import SeqRecord
 import gffutils
+from gffutils.gffwriter import GFFWriter
 import os
 import concurrent.futures
 # from Bio.Blast import
+from time import time
 
 # REMOVE!
 from parse_gene_presence_absence import read_gene_presence_absence
@@ -47,6 +49,7 @@ def extract_genome_fasta(gff_name):
     # Initialise the two return variables a dict for refound gene's annotaton and the largest locus_tag
     genome_fasta_dict = {}
     largest_locus_tag = ''
+    header_lines = []
 
     # Open the gff file and indicate that the FASTA sequence has not beed reached
     with open(gff_name, 'r') as gff_file:
@@ -88,8 +91,11 @@ def extract_genome_fasta(gff_name):
                         line = line.split('locus_tag=')[1]
                         if line > largest_locus_tag:
                             largest_locus_tag = line
+                # Save header lines
+                else:
+                    header_lines.append(line)
 
-    return genome_fasta_dict, largest_locus_tag
+    return genome_fasta_dict, largest_locus_tag, header_lines
 
 
 def construct_gff_line(gene_oi, genome_oi, contig, strand, annotation, refound_gene_tag, largest_locus_tag):
@@ -111,26 +117,101 @@ def construct_gff_line(gene_oi, genome_oi, contig, strand, annotation, refound_g
                f'0\t' \
                f'ID={new_locus_tag};annotation={annotation};locus_tag={new_locus_tag};old_locus_tag={refound_gene_tag}'
 
-    # Make gff line into gff feature line
-    gene_feature_line = gffutils.feature.feature_from_line(gff_line) # TODO: add the feature line to the gff database.
-
-    return gene_feature_line, new_locus_tag
+    return gff_line, new_locus_tag
 
 
-def annotate_refound_genes(gff_name, gene_data_dict, gene_pres_abs, temp_folder_path, annotation_dict):
+def add_gene_to_gff(tmp_gff, gene_oi, genome_oi, contig, strand, annotation, refound_gene_tag, largest_locus_tag):
+    # # Get path to temporary folder and where to put file
+    # tmp_gff = os.path.join(temp_folder_path, f'{gff_file_name.split(".gff")[0]}_tmp.gff')
+    #
+    # # Write gff file
+    # with open(tmp_gff, 'w') as gff_file:
+    #     for feature in gff_db.all_features():
+    #         gff_file.writelines(str(feature) + '\n')
+    #     gff_file.write(gff_line + '\n')
+    # # TODO - close file?
+    #
+    # os.remove(data_base)
+    # # Reload temporary gff file
+    # # TODO - Keep_order=False - speeds up this process!
+    # gffutils.create_db(tmp_gff, data_base, force=True, keep_order=False)
+    #
+    # # Attach gff file
+    # gff_db = gffutils.FeatureDB(data_base)
+
+    # TODO - append new gene to file that has been open and closed once to make less reads and writes...
+
+    # Open the file in append mode
+
+    # Add the gene line
+
+    # Close file
+    #TODO ####### CUT #########
+
+    # Prepare the start, end, and locus_tag of the gene feature line.
+    gene_start = genome_oi.find(gene_oi) + 1
+    gene_end = gene_start + len(gene_oi)
+    locus_tag_parts = largest_locus_tag.split('_')
+    locus_tag_parts[1] = int(locus_tag_parts[1]) + 1
+    new_locus_tag = f'{locus_tag_parts[0]}_{locus_tag_parts[1]}'
+
+    # construct tab delimited string containing the features of the gene
+    gff_line = f'{contig}\t' \
+               f'Panaroo\t' \
+               f'CDS\t' \
+               f'{gene_start}\t' \
+               f'{gene_end}\t' \
+               f'.\t' \
+               f'{strand}\t' \
+               f'0\t' \
+               f'ID={new_locus_tag};annotation={annotation};locus_tag={new_locus_tag};old_locus_tag={refound_gene_tag}'
+
+    with open(tmp_gff, 'a') as tmp_gff_file:
+        tmp_gff_file.write(gff_line + '\n')
+    tmp_gff_file.close()
+
+    return new_locus_tag
+
+
+def write_contig(file, contig_name, sequnce):
+    # Wrtie contig name
+    file.write(f'>{contig_name}\n')
+
+    # Write bulk of sequence
+    for i in range(len(sequnce) // 60):
+        file.write(sequnce[0+60*i:61+60*i] + '\n')
+
+    # Write remainder of sequence
+    remainder = len(sequnce) % 60
+    genome_length = len(sequnce)
+    file.write(sequnce[len(sequnce) - remainder:genome_length+1] + '\n')
+
+
+def annotate_refound_genes(gff_name, gene_data_dict, temp_folder_path, annotation_dict, corrected_gff_out_dir, i):
     """ Function to annotate the genes refound by Panaroo in a gff3 file"""
+    # Print info on progress
+    if (i+1) % 25 == 0 or i == 0:
+        print(f"Correcting GFF file #{i+1}")
+
     # Read in a gff file
     # Get base name of gff file and construct path to database in temporary folder
     gff_file_name = os.path.basename(gff_name)
-    data_base = os.path.join(f'{temp_folder_path}{gff_file_name}_db')
+    data_base = os.path.join(temp_folder_path, f'{gff_file_name}_db')
 
     # Create a database for the gff file
     gffutils.create_db(gff_name, data_base)
     # Attach database
     gff_db = gffutils.FeatureDB(data_base)
 
+    # Write quick tmp database for appending new genes.
+    tmp_gff = os.path.join(temp_folder_path, f'{gff_file_name.split(".gff")[0]}_tmp.gff')
+    with open(tmp_gff, 'w') as gff_file:
+        for feature in gff_db.all_features():
+            gff_file.writelines(str(feature) + '\n')
+    gff_file.close()
+
     # Pass the gff file manually to extract the genome fasta sequence(s) and the largest locus_tag
-    fasta_genome, largest_locus_tag = extract_genome_fasta(gff_name)
+    fasta_genome, largest_locus_tag, header_lines = extract_genome_fasta(gff_name)
 
     # Find all refound genes for given genome in gene data file
     genome_name = gff_file_name.split('.')[0]
@@ -139,62 +220,94 @@ def annotate_refound_genes(gff_name, gene_data_dict, gene_pres_abs, temp_folder_
     for refound_gene in gene_data_dict[genome_name].keys():
         gene_oi = gene_data_dict[genome_name][refound_gene]
 
-        for contig in fasta_genome.keys():
+        strand = None
+        contig_counter = 0
+        contigs = list(fasta_genome.keys())
+        while strand is None:
+            contig = contigs[contig_counter]
             genome_oi = fasta_genome[contig]
-            strand = None
 
             if gene_oi in genome_oi:
-                print("Found in contig")
                 strand = '+'
 
             else:
                 # get reverse complement of the gene
                 gene_oi = Seq.reverse_complement(gene_oi)
                 if gene_oi in genome_oi:
-                    print("Reversed - Found in contig")
                     strand = '-'
 
-            if strand is not None:
-                gff_line, largest_locus_tag = construct_gff_line(gene_oi, genome_oi, contig, strand,
+            contig_counter += 1
+
+        if strand is not None:
+            # Add the gene to the gff file.
+            largest_locus_tag = add_gene_to_gff(tmp_gff, gene_oi, genome_oi, contig, strand,
                                    annotation_dict[refound_gene], refound_gene, largest_locus_tag)
-                print(gff_line)
-                # gff_db = gff_db.update(data=gff_line, make_backup=False)
-                gff_db = gffutils.FeatureDB.update(self=gff_db, data=gff_line)
 
-            else:
-                raise ValueError(f"When correcting gff {gff_name}, a gene did not have any hit in the genome!")
+        else:
+            raise ValueError(f"When correcting gff {gff_name}, the gene: {refound_gene} "
+                             f"did not have any hit in the genome!")
 
-    # TODO - Add the annotation of the refound gene to the GFF file for the given genome. with contig, coordinates, annotation, locus_tag as .5 and old_locus_tag being the refound identifyer.
-        # TODO - if no contig is found then it's a complete genome. Find what the genome is called.
+    # Construct a database from the temporary gff that contain the added annotations
+    path_tmp_gff_db = os.path.join(temp_folder_path, f'{gff_file_name}_tmp_db')
+    # make database
+    gffutils.create_db(tmp_gff, path_tmp_gff_db)
+    # Attach database
+    tmp_gff_db = gffutils.FeatureDB(path_tmp_gff_db)
 
-    # TODO - Write the new GFF file.
+    # Print the final GFF3 file
+    with open(os.path.join(corrected_gff_out_dir, f'{gff_file_name.split(".gff")[0]}_corrected.gff'), 'w') as gff_file:
+        # Write initial lines
+        for line in header_lines:
+            gff_file.write(line)
 
-    # remove database for gff file in temporary folder
+        # ADD the gff lines
+        for feature in tmp_gff_db.all_features(order_by=('seqid', 'start')):
+            gff_file.writelines(str(feature) + '\n')
+
+        # Write line to seperate genome fasta
+        gff_file.write("##FASTA\n")
+
+        # Write the genome fasta
+        for contig_name in fasta_genome.keys():
+            write_contig(gff_file, contig_name, fasta_genome[contig_name])
+
+    # remove database for gff file and the temporary gff file in temporary folder
     os.remove(data_base)
+    os.remove(tmp_gff)
+    os.remove(path_tmp_gff_db)
 
 
-def correct_gffs(gffs, gene_data_file, gene_pres_abs, output_folder, annotation_dict):
+def correct_gffs(gffs, gene_data_file, output_folder, annotation_dict):
     # Read Gene_data.csv file into dict with a dict of refound genes for each genome
     gene_data_dict = read_gene_data(gene_data_file)
 
     # Construct temporary folder with write permission to store gff databases
     temp_folder_path = os.path.join(output_folder, 'genome_corer_tmp')
     os.mkdir(temp_folder_path)
+    # Construct directory to hold corrected gff files:
+    corrected_gff_out_dir = os.path.join(output_folder, 'Corrected_gff_files')
+    os.mkdir(corrected_gff_out_dir)
 
-    # TODO - multi process or thread commands below.
-    annotate_refound_genes('/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_genomes/GCA_000006785.gff',
-        gene_data_dict, None,
-        '/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/', annotation_dict)
+    # Multi process the annotation of the genomes. (Process has been found to be better than threads)
+    total_time = time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+        result = [executor.submit(annotate_refound_genes, gff, gene_data_dict, temp_folder_path,
+                                  annotation_dict, corrected_gff_out_dir, i) for i, gff in enumerate(gffs)]
+
+        for f in concurrent.futures.as_completed(result):
+            f.result()
 
     # Remove temporary database holding gff databases
     os.rmdir(temp_folder_path)
+    print(f"total time for correction {time() - total_time}")
+
+    return corrected_gff_out_dir
 
 
 if __name__ == '__main__':
     _, _, attribute_dict = read_gene_presence_absence('/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_pan_split_paralogs/gene_presence_absence_roary.csv',
                                                       1, 0.05)
 
-    correct_gffs(None, '/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_pan_split_paralogs/gene_data.csv',
-                     None, "/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests", attribute_dict)
+    correct_gffs(['/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_genomes/GCA_008694005.gff'], '/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_pan_split_paralogs/gene_data.csv',
+                     "/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests", attribute_dict)
     # genome_dict = extract_genome_fasta('/Users/mjespersen/OneDrive - The University of Melbourne/Phd/Parts/Recombination_hotspots/Code/Between_core_variation/data_for_unit_tests/test_pan_genome/50_refseq_genomes/GCA_000006785.gff')
-    # print(genome_dict)
