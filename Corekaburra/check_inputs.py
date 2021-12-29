e8aa5a59de141466f0eb20dd488a143174409653
milnus <44769523+milnus@users.noreply.github.com>
2021-12-29 15:03:52 +1100

    Add in check for the presence of the gene_data.csv file, if pan-genome is from Panaroo

--- Corekaburra/check_inputs.py
+++ Corekaburra/check_inputs.py
@@ -7,41 +7,10 @@ except ModuleNotFoundError:
     from exit_with_error import exit_with_error
 EXIT_INPUT_FILE_ERROR = 1
 
-
-def check_gff_files(file_list):
-    for file in file_list:
-        if not os.path.isfile(file):
-            raise FileNotFoundError(f'{file} can not be found!')
-    return True
-
-
-def check_gff_in_pan(file_list, gene_presence_absence_path):
-    with open(gene_presence_absence_path, 'r') as pan_file:
-        # Read the first line of the gene_presence_absence and extract the genome names
-        pan_header_line = pan_file.readline()
-        pan_header_line = pan_header_line.strip().split(',')
-        genome_names = pan_header_line[14:]
-
-
-    file_list = [os.path.basename(file) for file in file_list]
-    file_list_no_suffix = [file.rstrip('.gff') for file in file_list]
-
-    # Check if all or subset of GFFs from pan genome have been supplied,
-    # if only a subset then raise warning
-    if set(file_list).issubset(genome_names) or set(file_list_no_suffix).issubset(genome_names):
-        if len(file_list) < len(genome_names):
-            warnings.warn(
-                "Not all gff in pan genome given as input. I will run with it but are you sure this is deliberate?")
-
-        return True  # True used for unit testing
-
-    raise FileNotFoundError('Unexpected occurrence in the matching of input GFF files and the pan genome presence/absence file')
-
-
 def define_pangenome_program(folder):
     """
     Function to examine if input pan genome folder stems from Roary or Panaroo.
-    :param folder: Input folder provided as pan-genome folder.
+    :param folder: Input folder provided as pan-genome folder
     :return: The name of the program from which the pangenome is suspected to come from
     """
 
@@ -65,9 +34,37 @@ def define_pangenome_program(folder):
 
 
 def check_gene_data(folder):
-    """ Check if the gene_data.csv file is present in the folder from a Panaroo pan genome run. """
+    """
+    Check if the gene_data.csv file is present in the folder from a Panaroo pan-genome run
+    :param folder: Input folder provided as pan-genome folder
+    :return: Path to the identified gene_data.csv file
+    """
+
     if os.path.isfile(os.path.join(folder, 'gene_data.csv')):
         return os.path.join(folder, 'gene_data.csv')
     else:
-        raise FileNotFoundError('gene_data.csv file could not be located in the given pan genome input folder.\n'
-                                'Please give the -a flag to omit this step or locate the gene_data.csv file.')
+        exit_with_error('gene_data.csv file could not be located in the given pan genome input folder.\n'
+                        'Please give the -a flag to omit this step or locate the gene_data.csv file.',
+                        EXIT_INPUT_FILE_ERROR)
+
+
+def check_gff_in_pan(file_list, gene_presence_absence_path):
+    with open(gene_presence_absence_path, 'r') as pan_file:
+        # Read the first line of the gene_presence_absence and extract the genome names
+        pan_header_line = pan_file.readline()
+        pan_header_line = pan_header_line.strip().split(',')
+        genome_names = pan_header_line[14:]
+
+    file_list = [os.path.basename(file) for file in file_list]
+    file_list_no_suffix = [file.rstrip('.gff') for file in file_list]
+
+    # Check if all or subset of GFFs from pan genome have been supplied,
+    # if only a subset then raise warning
+    if set(file_list).issubset(genome_names) or set(file_list_no_suffix).issubset(genome_names):
+        if len(file_list) < len(genome_names):
+            warnings.warn(
+                "Not all gff in pan genome given as input. I will run with it but are you sure this is deliberate?")
+
+        return True  # True used for unit testing
+
+    raise FileNotFoundError('Unexpected occurrence in the matching of input GFF files and the pan genome presence/absence file')
